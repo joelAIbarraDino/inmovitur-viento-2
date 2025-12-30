64dbb7e38d7832b07a0a156cc4ceb3bdd094b688
joelAIbarraDino <joel.ibarra@dinozign.com>
2025-12-30 10:58:01 -0600

    correcion en paginador

--- resources/js/components/tableRecords/TablePagination.vue
+++ resources/js/components/tableRecords/TablePagination.vue
@@ -21,7 +21,7 @@ const props = defineProps<{
             as="a"
             v-for="(link, index) in props.links":key="index"
             :href="link.url??'#'"
-            :enabled="link.active"
+            :enabled="link.url ? true:false"
             v-html="link.label"
             :class="[
                 link.active ? 'bg-primary text-white':'bg-white text-gray-700',
